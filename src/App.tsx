18c3d2a9f3f29b6dc446c1c0bb057212f7623607
Juan David Lopez Ruiz <david.lopez112300@gmail.com>
2026-05-11 16:34:17 -0500

    Cambio URL
    se cambio el url en el app linea 56

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ export default function App() {
     formData.append('file', selectedImage);
 
     try {
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://blitz-casing-catalog.ngrok-free.dev', {
         method: 'POST',
         body: formData,
       });
